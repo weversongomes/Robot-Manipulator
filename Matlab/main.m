b70e56984d191a66301a7249e2f5b9e37a6474b0
Weverson <weverson_santos@live.com>
2018-12-10 12:59:03 -0300

    Valores em graus correspondem à posição física dos elos do robô relativa ao elo anterior.

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,6 +1,6 @@
-%s = serial('COM10','BaudRate',115200);
-%set(s,'Terminator','CR')
-%fopen(s);
+s = serial('COM10','BaudRate',115200);
+set(s,'Terminator','CR')
+fopen(s);
 
 while 1
    user_input = input('Enter the angles for all the six joints:');
@@ -11,8 +11,9 @@ while 1
    translated_values = translate_values(A);
       
    if validate_angles(translated_values)
-      command = strcat('#16 P ', int2str(translated_values(1)), ' #17 P ', int2str(translated_values(2)), ' #20 P ', int2str(translated_values(3)), ' #21 P ', int2str(translated_values(4)), ' #22 P ', int2str(translated_values(5)), ' #23 P ', int2str(translated_values(6)), ' T5000\n');
-      disp(command);
+      command = strcat('#16 P ', int2str(translated_values(1)), ' #17 P ', int2str(translated_values(2)), ' #20 P ', int2str(translated_values(3)), ' #21 P ', int2str(translated_values(4)), ' #22 P ', int2str(translated_values(5)), ' #23 P ', int2str(translated_values(6)), ' T3000\n');
+      %disp(command);
+      fprintf(s, '%s', command);
    end
    
    %if user_input >= 100 && user_input <= 3000
@@ -22,6 +23,6 @@ while 1
    %end
 end
 
-%fclose(s);
+fclose(s);
 
 %#ok<*ST2NM>
--- Matlab/validate_angles.m
+++ Matlab/validate_angles.m
@@ -3,13 +3,13 @@ function v = validate_angles(A)
 if A(1) < 500 || A(1) > 2500
     disp('Invalid value for joint 1');
     v = false;
-elseif A(2) < 600 || A(2) > 2100
+elseif A(2) < 830 || A(2) > 2100
     disp('Invalid value for joint 2');
     v = false;
 elseif A(3) < 1000 || A(3) > 2200
     disp('Invalid value for joint 3');
     v = false;
-elseif A(4) < 500 || A(4) > 2200
+elseif A(4) < 500 || A(4) > 2500
     disp('Invalid value for joint 4');
     v = false;
 elseif A(5) < 500 || A(5) > 2500
